fc21b58fcb7a7c71e23c8b557ae4eab02cc28228
EmilieSJKL612 <151094902+EmilieSJKL612@users.noreply.github.com>
2025-07-02 20:59:47 -0600

    try1's part two work

--- App.jsx
+++ App.jsx
@@ -8,5 +8,36 @@ import Cart from "./Cart";
 export default function App() {
   const [cart, setCart] = useState([]);
 
-  return <></>;
+  const addToCart = (plant) => {
+    setCart((prevCart) => {
+      const item = prevCart.find((p) => p.id === plant.id);
+      if (item) {
+        return prevCart.map((p) =>
+          p.id === plant.id ? { ...p, quantity: p.quantity + 1 } : p
+        );
+      } else {
+        return [...prevCart, { ...plant, quantity: 1 }];
+      }
+    });
+
+};
+
+const updateQuantity = (plantId, amount) => {
+  setCart((prevCart) => {
+    return prevCart
+      .map((item) =>
+        item.id === plantId ? { ...item, quantity: item.quantity + amount } : item
+      )
+      .filter((item) => item.quantity > 0);
+  });
+};
+
+return (
+  <div className="container">
+    <h1>Proper Plants</h1>
+    <PlantList plants={PLANTS} onAddToCart={addToCart} />
+    <Cart cart={cart} onUpdateQuantity={updateQuantity} />
+  </div>
+);
+
 }
